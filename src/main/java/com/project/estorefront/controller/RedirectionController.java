08cf5c04fd3fcc474e45fc4580bbde4e6aaa5963
Hrishi <hrishi.patel@dal.ca>
2022-07-02 20:44:31 -0300

    add more logic to RedirectionController

--- src/main/java/com/project/estorefront/controller/RedirectionController.java
+++ src/main/java/com/project/estorefront/controller/RedirectionController.java
@@ -3,11 +3,28 @@ package com.project.estorefront.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class RedirectionController {
 
     @GetMapping("/")
-    public String redirectToLogin() {
-        return "redirect:/login";
+    public String redirectToLogin(HttpSession session) {
+
+        String role = (String) session.getAttribute("role");
+        String userID = (String) session.getAttribute("userID");
+
+        if (role == null || userID == null) {
+            return "redirect:/login";
+        } else {
+            if (role.equals("seller")) {
+                return "redirect:/seller";
+            } else if (role.equals("buyer")) {
+                return "redirect:/buyer";
+            } else {
+                session.invalidate();
+                return "redirect:/login";
+            }
+        }
     }
 }
