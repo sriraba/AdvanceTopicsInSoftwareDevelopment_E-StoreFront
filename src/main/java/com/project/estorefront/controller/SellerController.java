0f12488295117c583451cfe7556ba76aa9d5195d
Hrishi <hr581440@dal.ca>
2022-07-14 15:40:16 -0300

    merge CouponController to SellerController

--- src/main/java/com/project/estorefront/controller/SellerController.java
+++ src/main/java/com/project/estorefront/controller/SellerController.java
@@ -1,14 +1,12 @@
 package com.project.estorefront.controller;
 
 import com.project.estorefront.model.*;
+import com.project.estorefront.repository.CouponsPersistence;
 import com.project.estorefront.repository.IInventoryItemPersistence;
 import com.project.estorefront.repository.InventoryItemPersistence;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
@@ -97,6 +95,39 @@ public class SellerController {
         item.setItemID(itemID);
         inventoryItemPersistence.delete(item);
         return "seller-items";
+    }
+
+    @GetMapping("/seller/coupons")
+    public String view(Model model) {
+        CouponsPersistence persistenceObj = new CouponsPersistence();
+        model.addAttribute("coupons", persistenceObj.getCoupons());
+        return "view-coupons";
+    }
+
+    @GetMapping("/seller/add-coupon")
+    public String add() {
+        return "add-coupon";
+    }
+
+    @PostMapping("/create-coupon")
+    public String create(@RequestParam("name") String couponName, @RequestParam("amount") String amount, @RequestParam("percent") String percent) {
+        CouponsPersistence persistenceObj = new CouponsPersistence();
+
+        int id = persistenceObj.getCoupons().size() + 1;
+
+        Coupon coupon = new Coupon(id, couponName, Double.parseDouble(amount), Double.parseDouble(percent));
+        persistenceObj.saveCoupon(coupon);
+
+        return "redirect:/coupons";
+    }
+
+    @RequestMapping(value= "/seller/coupons/view/{id}", method = RequestMethod.GET)
+    public String view_details(@PathVariable("id") int id, Model model ) {
+
+        CouponsPersistence persistenceObj = new CouponsPersistence();
+        model.addAttribute("coupon", persistenceObj.getCouponById(id));
+
+        return  "coupon-detail";
     }
 
 }
