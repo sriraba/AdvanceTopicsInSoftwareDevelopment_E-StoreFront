f6a45677f59931bfb3c5b982c258b60e17f63b0d
Hrishi <hr581440@dal.ca>
2022-07-20 14:52:35 -0300

    add Crypto interface and class

---
+++ src/main/java/com/project/estorefront/model/Crypto.java
@@ -0,0 +1,13 @@
+package com.project.estorefront.model;
+
+public class Crypto implements  ICrypto{
+    @Override
+    public String encrypt(String value) {
+        return null;
+    }
+
+    @Override
+    public String decrypt(String value) {
+        return null;
+    }
+}
---
+++ src/main/java/com/project/estorefront/model/ICrypto.java
@@ -0,0 +1,10 @@
+package com.project.estorefront.model;
+
+public interface ICrypto {
+
+    String encrypt(String value);
+
+    String decrypt(String value);
+
+
+}
