011bd0fdb466434aca9fe4a8377ed069ba1765f3
Shamsheer Singh <sh251568@dal.ca>
2022-07-26 17:17:28 -0300

    Fixed tests with cartValidator

--- src/main/java/com/project/estorefront/model/validators/CartValidator.java
+++ src/main/java/com/project/estorefront/model/validators/CartValidator.java
@@ -20,10 +20,13 @@ public class CartValidator implements ICartValidator {
         for (IInventoryItem item : cart.getCartItems()) {
             IInventoryItem dbItem = dbItems.getItemByID(item.getItemID());
 
-            if (dbItem.getItemQuantity() < item.getItemQuantity()) {
-                errors += "Error: Only " + dbItem.getItemQuantity() + " pcs. are available in stock for " + item.getItemName();
-                errors += ". Kindly change the quantity to place order!";
-                break;
+            if(dbItem != null)
+            {
+                if (dbItem.getItemQuantity() < item.getItemQuantity()) {
+                    errors += "Error: Only " + dbItem.getItemQuantity() + " pcs. are available in stock for " + item.getItemName();
+                    errors += ". Kindly change the quantity to place order!";
+                    break;
+                }
             }
 
         }
